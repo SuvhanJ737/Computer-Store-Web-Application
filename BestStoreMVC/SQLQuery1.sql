9f208db9993e30795cacb956bc53d8d010f30232
suvah <suvahnjuglall@outlook.com>
2024-04-15 10:45:58 +0200

    Final Commit

---
+++ BestStoreMVC/SQLQuery1.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO Products (Name, Brand, Category, Price, Description, ImageFileName, CreatedAt) VALUES
+('Superfly', 'Dell', 'computer', 6000, '8 RAM 1TB', '1000.jpg', GETDATE());
---
+++ BestStoreMVC/Services/ApplicationDbContent.cs
@@ -0,0 +1,16 @@
+﻿using BestStoreMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestStoreMVC.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
